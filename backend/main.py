12c9391af1decff532841ff33cb83783a93c3ec7
Taylor Yee <tayloryee1@gmail.com>
2026-08-17 22:02:08 -0700

    setup frontend, basic profile screen

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.match import router as matchRouter
 from routes.player import router as playerRouter
 
 
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(playerRouter)
 app.include_router(matchRouter)
